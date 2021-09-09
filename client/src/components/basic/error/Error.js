b1485d562b1737039785a05d184b38351f8b68bb
soowan-jang <jangswn2@gmail.com>
2021-09-09 23:41:35 +0900

    [Client] #46 feat:footer code add

---
+++ client/src/components/basic/error/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <img href="./401Error.svg"></img>
+            <img href="./404Error.svg"></img>
+        </div>
+    );
+};
+
+export default Error;
