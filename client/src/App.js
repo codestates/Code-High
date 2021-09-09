8cea0e9de24963302ebb8a8c93d157de6d014372
siyoon <jeoncue@gmail.com>
2021-09-09 15:54:31 +0900

    [Client] #20 feat:setting without CRA

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainBanner from './components/landing/MainBanner';
+
+function App(){
+  return (
+    <MainBanner />
+  );
+};
+
+export default App;
---
+++ client/src/components/landing/MainBanner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './_MainBanner.scss';
+
+function MainBanner(){
+  return (
+    <div className='test'>Hello Code-High</div>
+  );
+};
+
+export default MainBanner;
