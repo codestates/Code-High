47c5a72c5ce0218fb4205fe9f8b1316c5709e55a
soyou <loverduck97@naver.com>
2021-09-13 23:30:10 +0900

    [Server] feat: add router

---
+++ src/routes/post.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+const postRouter = Router();
+
+postRouter.get('/');
+postRouter.patch('/');
+postRouter.delete('/');
+
+export default postRouter;
--- src/routes/sign.ts
+++ src/routes/sign.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import * as signController from '../controllers/signController'
 const signRouter = Router();
 
-signRouter.get('/');
-signRouter.patch('/');
-signRouter.delete('/');
+signRouter.get('/email', signController.emailLogin);
+signRouter.post('/kakao', signController.kakaoLogin);
+signRouter.post('/google', signController.googleLogin);
+signRouter.post('/github', signController.githubLogin);
+signRouter.post('/signup', signController.signUpEmail);
+signRouter.get('/logout', signController.logout);
+signRouter.post('/checkemail?', signController.checkEmail);
 
 export default signRouter;
