627565f8c79f18ac61ba362355797f0887d608e1
soyou <loverduck97@naver.com>
2021-09-13 14:27:54 +0900

    [Server] fix: setting aws parameter

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu/Code-High
 
+export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+export REFRESH_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names REFRESH_SECRET --query Parameters[0].Value | sed 's/"//g')
+
 npm run --script build
 authbind --deep pm2 start dist/index.js
