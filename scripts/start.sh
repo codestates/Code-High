e3b48aa83bb2f956e41c4662b9b09b25be507425
soyou <loverduck97@naver.com>
2021-09-13 10:16:15 +0900

    [Server] feat: express server and delete aws codedeploy setting

--- scripts/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/Code-High
-
-npm run --script build
-authbind --deep pm2 start dist/main.js
--- scripts/stop.sh
+++
@@ -1,3 +0,0 @@
-cd /home/ubuntu/Code-High
-pm2 stop dist/main.js 2> /dev/null || true
-pm2 delete dist/main.js 2> /dev/null || true
