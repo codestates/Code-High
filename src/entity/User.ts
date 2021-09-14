fb35b369bf311e921648c14c3283e21edbf74847
edan_3000 <roseswallow13@gmail.com>
2021-09-14 13:23:54 +0900

    [Server] #72 fix: entity optional

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -15,7 +15,7 @@ export class User extends BaseEntity {
   @Column({ type: 'varchar'})
   name: string;
 
-  @Column({ type: 'varchar'})
+  @Column({ type: 'varchar', nullable: true})
   phone: string;
 
   @Column({ type: 'varchar'})
