b99293d62a32cc79ed2edb760bac475825dc6eb6
soyou <loverduck97@naver.com>
2021-09-14 11:35:04 +0900

    [Server] feat: add signup

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,6 @@
-import { sendEmail } from './mail';
+import { Request, Response } from 'express';
+import { User } from '../entity/User';
+import * as bcrypt from 'bcrypt';
 
 
 export const emailLogin = () => {
@@ -21,8 +23,26 @@ export const logout = () => {
 
 }
 
-export const signUpEmail = () => {
+export const signUpEmail = async (req: Request, res: Response) => {
+  const { email, password, name, phone } = req.body;
+  // 필수 정보 확인
+  if (!email || !password || !name) {
+    return res.status(422).send('Unprocessable Ent');
+  }
 
+  // 중복 이메일 확인
+  const userEmail = await User.findOne({ where: { email } })
+  if (userEmail) {
+    return res.status(409).send('Email Conflict');
+  }
+
+  const hashPwd = await bcrypt.hash(password, 10);
+  const userInfo = await User.create({
+    email,
+    password,
+    name,
+    phone
+  });
 }
 
 export const checkEmail = () => {
