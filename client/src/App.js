27569f62f1f9804317d897c633d597a840d7e5f7
edan_3000 <roseswallow13@gmail.com>
2021-09-15 23:55:59 +0900

    [Client] #13 feat:oauth

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import SignIn from './components/basic/modal/SignIn';
-=======
 import CodeInput from './pages/CodeInput';
->>>>>>> 96826dec633ac6f9d0b9cec9e187bc1167bdad6d
 // import Landing from './pages/Landing';
 import SideBar from './components/basic/navbar/SideBar';
 import NavBar from './components/basic/navbar/NavBar'
@@ -16,23 +13,15 @@ import Landing from './pages/Landing';
 
 function App(){
   return (
-<<<<<<< HEAD
-=======
 
     <CodeInput />
->>>>>>> 96826dec633ac6f9d0b9cec9e187bc1167bdad6d
     // <CodeStorage/>
     // <Landing />
     // <SideBar/>
     // <NavBar/>
     // <BlackFooter/>
     // <WhiteFooter/>
-<<<<<<< HEAD
-    // <Tag/>
-    <SignIn />
-=======
     
->>>>>>> 96826dec633ac6f9d0b9cec9e187bc1167bdad6d
   );
 };
 
