25112d40b6b71c8a8ef85e262a9020d71c3b40bd
soyou <loverduck97@naver.com>
2021-09-21 16:05:06 +0900

    [Server] fix: aws deploy setting

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,5 +7,11 @@ export EMAIL_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names EMA
 export NODEMAILER_USER=$(aws ssm get-parameters --region ap-northeast-2 --names NODEMAILER_USER --query Parameters[0].Value | sed 's/"//g')
 export NODEMAILER_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names NODEMAILER_PASSWORD --query Parameters[0].Value | sed 's/"//g')
 
+export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_USER=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+
 npm run --script build
 authbind --deep pm2 start dist/index.js
