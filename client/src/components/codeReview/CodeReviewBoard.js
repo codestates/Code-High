3394c8a69d08cb0e7be13fe384d39a8c3e10e1f5
Siyoon Jeon <jeoncue@gmail.com>
2021-09-24 17:06:04 +0900

    [Client] #201 modify:remove component and modify scroll event

--- client/src/components/codeReview/CodeReviewBoard.js
+++ client/src/components/codeReview/CodeReviewBoard.js
@@ -4,38 +4,55 @@ import { resetCodereviewPost } from '../../redux/actions/codePostActions';
 import { useSelector, useDispatch } from 'react-redux';
 import SearchInput from '../basic/search/SearchInput';
 
-function CodeReviewBoard () {
-  const state = useSelector(state => state.codePostReducer);
-  const userState = useSelector(state => state.userReducer);
-  const { postList } = state;
+function CodeReviewBoard() {
+  const [count, setCount] = useState(2);
+  const state = useSelector((state) => state.codePostReducer);
+  const userState = useSelector((state) => state.userReducer);
+  const { postList, addList } = state;
   const { userInfo } = userState;
-
   const dispatch = useDispatch();
 
-  //!새로고침하면 첫 15개만 나타남
-  useEffect(()=>{
-    dispatch(resetCodereviewPost())
-  },[])
-  console.log('코드리뷰보드에서의 코드리스트', postList)
+  //!새로고침 시, 스크롤 상단
+  window.onload = function () {
+    setTimeout(() => {
+      scrollTo(0, 0);
+    }, 100);
+  };
 
+  //!새로고침하면 첫 15개만 나타남
+  useEffect(() => {
+    dispatch(resetCodereviewPost());
+  }, []);
+  console.log('코드리뷰보드에서의 코드리스트', postList);
+
+  const getMorePost = async () => {
+    dispatch(getReviewPost(count));
+    setCount(count + 1);
+    // if (addList.length !== 0 && addList.length <= 6) {
+    //   const addContent = document.createElement('div');
+    //   addContent.classList.add('codereviewboard-card');
+    //   addContent.innerHTML = `{addList.map((item) => {
+    //         return (<h1>item.title</h1>
+    //             <div>item.codeContent</div>);
+    //     })}`;
+    //   document.querySelector('section').appendChild(addContent);
+    // } else {
+    //   return;
+    // }
+  };
+  console.log(count);
   const onScroll = (e) => {
     //window.innerHeight=1006 , window.scrollY=1, document.body.offsetHeight=1007
-    console.log(window.innerHeight , window.scrollY, document.body.offsetHeight)
-    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-      setTimeout(() => {
-        let count = 1;
-        const addContent = document.createElement("div");
-        addContent.classList.add("codereviewboard-card")
-        // dispatch(getReviewPost(count+1))
-        addContent.innerHTML = `{postList.map((item) => {
-          return (<h1>item.title</h1>
-              <div>item.codeContent</div>);
-      })}`
-        document.querySelector('section').appendChild(addContent);
-      },1000)
+    // console.log(document.querySelector('section').scrollTop);
+    // console.log(e.target.scrollingElement.scrollTop);
+    // console.log(window.innerHeight, window.scrollY, document.body.offsetHeight);
+    const { clientHeight, scrollTop, scrollHeight } = e.target.scrollingElement;
+    if (scrollTop + clientHeight === scrollHeight) {
+      getMorePost();
     }
-  }
+  };
 
+  document.addEventListener('scroll', onScroll);
 
   return (
     <div className='codereviewboard'>
@@ -43,13 +60,16 @@ function CodeReviewBoard () {
         <div className='codereviewboard-header'>
           <SearchInput />
         </div>
-        <section className='codereviewboard-cardbox' onScroll={onScroll}>
+        <section className='codereviewboard-cardbox'>
           {postList.map((item) => {
-              return (<div className='codereviewboard-card'>
-                  <h1>{item.title}</h1>
-                  <div>{item.codeContent}</div>
-                </div>);
+            return (
+              <div className='codereviewboard-card'>
+                <h1>{item.title}</h1>
+                <div>{item.codeContent}</div>
+              </div>
+            );
           })}
+          {/* <div className='codereviewboard-loding'>로딩중</div> */}
         </section>
       </div>
     </div>
