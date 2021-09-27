777371c8c9bdae54dd345441ff8bd7f4b9f5f6a2
Soyou <loverduck97@naver.com>
2021-09-27 11:20:30 +0900

    Delete menu.ts

--- src/controllers/menu.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express'
-import { Menu } from '../entity/Menu';
-
-const menuList = (req: Request, res: Response) => {
-  res.status(200).send({ message: 'menu'})
-}
-
-export { menuList }
