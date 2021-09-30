e9c98699115c673fd8f947b6b067d4cefc28c0fb
soyou <loverduck97@naver.com>
2021-09-30 16:20:54 +0900

    [Server] fix: checkAuth middleware

--- src/middleware/checkAuth.ts
+++ src/middleware/checkAuth.ts
@@ -3,9 +3,8 @@ import { User } from '../entity/User';
 import { generateloginToken, verifyAccessToken, verifyRefreshToken } from '../utils/jwt';
 
 export const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
-  let accessToken = req.headers['authorization'].split(' ')[1];
+  let accessToken = req.headers['authorization'] ? req.headers['authorization'].split(' ')[1] : 'undefined';
   // 로그인하지않은 사용자
-  console.log(accessToken, typeof accessToken);
   if (accessToken === 'undefined') return next();
 
   const tokenInfo: any = verifyAccessToken(accessToken);
