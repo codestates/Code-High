8ad9abfbbc47b0045bfb86e99bd53633fbc6dd25
Siyoon Jeon <jeoncue@gmail.com>
2021-10-01 14:10:28 +0900

    [Client] #278 feat:markdown

--- client/src/components/CodePost/PostMainView.js
+++ client/src/components/CodePost/PostMainView.js
@@ -1,52 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import MDEditor from '@uiw/react-md-editor';
+import CodeEditor from '@uiw/react-textarea-code-editor';
 
 const PostMain = () => {
   const userState = useSelector((state) => state.userReducer);
   const { userInfo } = userState;
   const postState = useSelector((state) => state.codePostReducer);
   const { codePost } = postState;
+  const [codeLanguage, setCodeLanguage] = useState('js')
 
-  console.log(codePost)
+  console.log(codePost.postTags.language[0].name);
+
+useEffect(()=>{
+  if(codePost.postTags.language[0].name==='C#'){
+    setCodeLanguage('cs')
+  } else if(codePost.postTags.language[0].name==='C++'){
+    setCodeLanguage('cpp')
+  } else {
+    setCodeLanguage(codePost.postTags.language[0].name)
+  }
+},[])
 
   return (
     <div className='codepostmain'>
       <div className='codepostview-container'>
         <span className='codepostview-code'>
-        <MDEditor.Markdown
-          source={`${codePost.codeContent}`}
-          previewOptions={{
-            components: {
-              code: ({ inline, children, className, ...props }) => {
-                const txt = children[0] || '';
-                if (inline) {
-                  if (typeof txt === 'string' && /^\$\$(.*)\$\$/.test(txt)) {
-                    const html = katex.renderToString(
-                      txt.replace(/^\$\$(.*)\$\$/, '$1'),
-                      {
-                        throwOnError: false,
-                      }
-                    );
-                    return <code dangerouslySetInnerHTML={{ __html: html }} />;
-                  }
-                  return <code>{txt}</code>;
-                }
-                if (
-                  typeof txt === 'string' &&
-                  typeof className === 'string' &&
-                  /^language-katex/.test(className.toLocaleLowerCase())
-                ) {
-                  const html = katex.renderToString(txt, {
-                    throwOnError: false,
-                  });
-                  return <code dangerouslySetInnerHTML={{ __html: html }} />;
-                }
-                return <code className={String(className)}>{txt}</code>;
-              },
-            },
-          }}
-        />
+          <CodeEditor
+            readOnly
+            value={codePost.codeContent}
+            language={codeLanguage}
+            style={{
+              fontSize: 15,
+              backgroundColor: '#f5f5f5',
+              fontWeight:500,
+              fontFamily:
+                'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+            }}
+          />
         </span>
         <span className='codepostview-text'>{codePost.textContent}</span>
       </div>
