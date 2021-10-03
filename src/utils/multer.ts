3e61899efe8889bf26781cfbe38488e101937c38
soyou <loverduck97@naver.com>
2021-10-03 22:55:33 +0900

    [Server] fix: remove multer

--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -1,26 +1,26 @@
-import * as multer from 'multer';
-import * as multerS3 from 'multer-s3';
-import * as aws from 'aws-sdk';
-import { Request } from 'express';
-import 'dotenv/config';
+// import * as multer from 'multer';
+// import * as multerS3 from 'multer-s3';
+// import * as aws from 'aws-sdk';
+// import { Request } from 'express';
+// import 'dotenv/config';
 
-aws.config.update({
-  "accessKeyId": process.env.S3_ACCESS_ID,
-  "secretAccessKey": process.env.S3_SECRET_KEY,
-  "region": process.env.S3_REGION
-})
+// aws.config.update({
+//   "accessKeyId": process.env.S3_ACCESS_ID,
+//   "secretAccessKey": process.env.S3_SECRET_KEY,
+//   "region": process.env.S3_REGION
+// })
 
-const s3 = new aws.S3();
+// const s3 = new aws.S3();
 
-export const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    bucket: 'code-high-image',
-    acl: 'public-read',
-    key: (req: Request, file, cb) => {
-      cb(null, Date.now() + '.' + file.originalname.split('.').pop());
-    }
-  }),
-  limits: { fileSize: 1000 * 1000 * 10 }
-});
+// export const upload = multer({
+//   storage: multerS3({
+//     s3: s3,
+//     contentType: multerS3.AUTO_CONTENT_TYPE,
+//     bucket: 'code-high-image',
+//     acl: 'public-read',
+//     key: (req: Request, file, cb) => {
+//       cb(null, Date.now() + '.' + file.originalname.split('.').pop());
+//     }
+//   }),
+//   limits: { fileSize: 1000 * 1000 * 10 }
+// });
