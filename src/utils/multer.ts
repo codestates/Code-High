08794acd311791d6a0495aaba0bf5f2862b6cd8f
soyou <loverduck97@naver.com>
2021-10-05 15:37:09 +0900

    [Server] fix: deploy test

--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -1,16 +1,16 @@
 // import * as multer from 'multer';
 // import * as multerS3 from 'multer-s3';
-import * as aws from 'aws-sdk';
+// import * as aws from 'aws-sdk';
 // import { Request } from 'express';
-import 'dotenv/config';
+// import 'dotenv/config';
 
-aws.config.update({
-  "accessKeyId": process.env.S3_ACCESS_ID,
-  "secretAccessKey": process.env.S3_SECRET_KEY,
-  "region": process.env.S3_REGION
-})
+// aws.config.update({
+//   "accessKeyId": process.env.S3_ACCESS_ID,
+//   "secretAccessKey": process.env.S3_SECRET_KEY,
+//   "region": process.env.S3_REGION
+// })
 
-const s3 = new aws.S3();
+// const s3 = new aws.S3();
 
 // export const upload = multer({
 //   storage: multerS3({
