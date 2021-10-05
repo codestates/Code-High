fa609f9640836847f8218d6ada0a6d462e09d1c9
edan_3000 <roseswallow13@gmail.com>
2021-10-05 16:38:47 +0900

    [Client] #354 feat: mypage password modify

--- client/src/components/basic/modal/ModifyUser.js
+++ client/src/components/basic/modal/ModifyUser.js
@@ -1,82 +1,81 @@
-import React, { useRef, useState }from 'react';
+import React, { useRef, useState, useEffect }from 'react';
 import modifyimg from '../../../images/modifyimg.png';
 import codehighlogo from '../../../images/codehighlogo.png';
 import { useSelector, useDispatch } from 'react-redux';
-import { modifyUser } from '../../../redux/actions/userActions'
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
-function ModifyUser ({userInfoPopUp,setShowUserInfoPopUp}) {
+function ModifyUser({ userInfoPopUp, setShowUserInfoPopUp }) {
+  const ModifyUserBackgroundEl = useRef(null);
   const state = useSelector((state) => state.userReducer);
   const { userInfo } = state;
-  const ModifyUserBackgroundEl = useRef(null);
-  const [newPassword, setNewPassword] = useState('');
-  const [reNewPassword, setReNewPassword] = useState('');
-  const [newNickName, setNewNickName] = useState('');
-  const dispatch = useDispatch();
   const history = useHistory();
   const serverUrl = 'https://api.codehigh.club';
+  const [signupNotice, setSignupNotice] = useState('비밀번호를 입력해 주세요');
+  const [modifyInfo, setModifyInfo] = useState({
+    password: '',
+    passwordcheck: '',
+    nickname: userInfo.name
+  })
 
   const ModifyUserBackgroundClick = (e) => {
     if (e.target === ModifyUserBackgroundEl.current) {
-        setShowUserInfoPopUp(!userInfoPopUp);
-    }
-};
-  
-
-  const handlemodify = (e) => {
-    let changedClass = e.target.className;
-    if (changedClass === 'newpassword') {
-      setNewPassword(e.target.value);
-    } else if (changedClass === 'renewpassword') {
-      setReNewPassword(e.target.value);
-    } 
-    if (changedClass === 'nickname') { 
-      setNewNickName(e.target.value);
-    }
-    
-    axios
-      .patch(
-        `${serverUrl}/user`,
-        { password: newPassword },
-        {
-          headers: { 'Content-Type': 'application/json' },
-        }
-      )
-      .then((data) => {
-        if (data.status === 200) {
-          history.push('/');
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    
-  }
-
-  // const handleButtonClickModify = () => {
-  //   if (newPassword === reNewPassword && newPassword !== undefined && newPassword !== '') {
-  //     const data = {
-  //       accessToken: userInfo ? userInfo.accessToken : undefined,
-  //       password : newPassword
-  //     };
-  //     console.log(data);
-  //     dispatch(modifyUser(data));
-  //     history.push('/');
-  //   }
-  //   console.log(newPassword, 'newPassword');
-  //   console.log(reNewPassword, 'reNewPassword');
-  //   console.log('수정');
-  // }
-
-  const enterKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      handlemodify();
+      setShowUserInfoPopUp(!userInfoPopUp);
     }
   };
 
+  const handleInputValue = (key) => (e) => {
+    setModifyInfo({ ...modifyInfo, [key]: e.target.value });
+  };
 
-    
-  
+  const handlemodify = () => {
+    const { newpassword, passwordcheck, nickname } = modifyInfo;
+    if (newpassword !== passwordcheck) {
+      setSignupNotice('비밀번호가 일치하지 않습니다.');
+      return;
+    } else if (newpassword === passwordcheck && newpassword === '') {
+      axios
+        .patch(
+          `${serverUrl}/user`,
+          { name: nickname },
+          {
+            headers: { Authorization: `bearer ${userInfo.accessToken}` },
+          }
+        )
+        .then((data) => {
+          if (data.status === 200) {
+            history.push('/');
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else if (newpassword === passwordcheck) {
+      axios
+        .patch(
+          `${serverUrl}/user`,
+          { password: newpassword, name: nickname },
+          {
+            headers: { Authorization: `bearer ${userInfo.accessToken}` },
+          }
+        )
+        .then((data) => {
+          if (data.status === 200) {
+            history.push('/');
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+
+  const enterKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handlemodify(e);
+    }
+  };
+
   return (
     <div className='modifyuser-modal'>
       <div className='modifyuser-modal-overlay'
@@ -94,14 +93,14 @@ function ModifyUser ({userInfoPopUp,setShowUserInfoPopUp}) {
             <div>이메일</div>
             <input placeholder={userInfo.email} disabled />
             <div>비밀번호</div>
-            <input className='newpassword' type='password' placeholder='변경할 비밀번호를 입력해주세요.' onKeyPress={enterKeyPress}/>
+            <input type='password' placeholder='변경할 비밀번호를 입력해주세요.' onChange={handleInputValue('newpassword')} onKeyPress={enterKeyPress} />
             <div>비밀번호 확인</div>
-            <input className='renewpassword' type='password' placeholder='변경할 비밀번호를 확인해주세요.' onKeyPress={enterKeyPress}/>
+            <input type='password' placeholder='변경할 비밀번호를 확인해주세요.' onChange={handleInputValue('passwordcheck')} onKeyPress={enterKeyPress} />
             <div>닉네임</div>
-            <input className='nickname' placeholder='변경할 닉네임을 입력해주세요.' onKeyPress={enterKeyPress}/>
+            <input placeholder='변경할 닉네임을 입력해주세요.' onChange={handleInputValue('nickname')} onKeyPress={enterKeyPress} />
           </article>
           <div className='modifyuser-button-container'>
-            <button type='submit' onClick={enterKeyPress}>회원정보 수정</button>
+            <button type='submit' onClick={handlemodify}>회원정보 수정</button>
           </div>
         </div>
       </div>
