68bc73191c24e53c0de4c089e20e8a03fb1ef5fb
soyou <loverduck97@naver.com>
2021-10-05 16:11:28 +0900

    [Server] modify: email html

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -44,13 +44,13 @@ export const sendSignUpEmail = async (email: string, name: string, token: string
   }
 </style>
 
-<table border="0" cellpadding="0" cellspacing="0" width="100%">
+<table border="0" cellpadding="0" cellspacing="0" width="500px">
   <tr>
     <td bgcolor="#ffffff" style="font-size: 0; line-height: 0; height: 0;" height="0">
       <table>
         <tr>
-          <td border="0" cellpadding="0" cellspacing="0" width="100%">
-            <img src="https://cdn.discordapp.com/attachments/879193560598061087/887501955226816552/codehigh.png" style="display: block;" alt="Some image" height="50" weight="100">
+          <td border="0" cellpadding="0" cellspacing="0" width="500px">
+            <img src="https://cdn.discordapp.com/attachments/879193560598061087/887501955226816552/codehigh.png" style="display: flex; margin-top: 20px; margin-left: 20px;" alt="Some image" height="50" weight="100">
           </td>
         </tr>
       </table>
@@ -59,20 +59,20 @@ export const sendSignUpEmail = async (email: string, name: string, token: string
   
   <tr>
     <td>
-      <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <table border="0" cellpadding="0" cellspacing="0" width="500px" style="padding: 20px; margin: 20px" bgcolor="#E1E1E1">
         <tr>
           <td>
             <div>
-              안녕하세요 ${name}님 <br/>
+              안녕하세요, ${name}님.<br/>
               아래 버튼을 눌러 이메일 인증을 완료해주세요.<br/>
-              이 링크는 5분간 유효합니다.
+              해당 링크는 5분간 유효합니다.
             </di>
-            <div align="center" style="padding: 20px;">
+            <div align="left" style="padding: 20px; margin: 20px" >
               <a href=${redirectURL} class="btn-ctf" target="_blank">이메일 인증</a>
             </div>
             <div>
-              <div>버튼이 보이지 않는다면, 위 주소로 접속해 이메일 인증을 진행해주세요.<br>
-              <div>${redirectURL}</div>
+              <div>버튼이 보이지 않는다면,<br>아래 주소로 접속해 이메일 인증을 진행해주세요.<br>감사합니다.
+              <div style="margin-top: 10px">${redirectURL}</div>
               </div>
             </div>
           </td>
@@ -139,13 +139,13 @@ export const sendPasswordEmail = async (email: string, code: string) => {
   }
 </style>
 
-<table border="0" cellpadding="0" cellspacing="0" width="100%">
+<table border="0" cellpadding="0" cellspacing="0" width="500px">
   <tr>
     <td bgcolor="#ffffff" style="font-size: 0; line-height: 0; height: 0;" height="0">
       <table>
         <tr>
-          <td border="0" cellpadding="0" cellspacing="0" width="100%">
-            <img src="https://cdn.discordapp.com/attachments/879193560598061087/887501955226816552/codehigh.png" style="display: block;" alt="Some image" height="50" weight="100">
+          <td border="0" cellpadding="0" cellspacing="0" width="500px">
+            <img src="https://cdn.discordapp.com/attachments/879193560598061087/887501955226816552/codehigh.png" style="display: flex; margin-top: 20px; margin-left: 20px;" alt="Some image" height="50" weight="100">
           </td>
         </tr>
       </table>
@@ -154,19 +154,20 @@ export const sendPasswordEmail = async (email: string, code: string) => {
   
   <tr>
     <td>
-      <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <table border="0" cellpadding="0" cellspacing="0" width="500px" style="padding: 20px; margin: 20px" bgcolor="#E1E1E1">
         <tr>
           <td>
             <div>
-              아래 버튼을 클릭해 비밀번호 재설정을 진행해주세요.<br/>
-              이 링크는 5분간 유효합니다.
+              안녕하세요.<br/>
+              아래 버튼을 눌러 비밀번호 재설정을 진행해주세요.<br/>
+              해당 링크는 5분간 유효합니다.
             </di>
-            <div align="center" style="padding: 20px;">
+            <div align="left" style="padding: 20px; margin: 20px" >
               <a href=${redirectURL} class="btn-ctf" target="_blank">비밀번호 재설정</a>
             </div>
             <div>
-              <div>버튼이 보이지 않는다면, 위 주소로 접속해 비밀번호 변경을 진행해주세요.<br>
-              <div>${redirectURL}</div>
+              <div>버튼이 보이지 않는다면,<br>아래 주소로 접속해 비밀번호 재설정을 진행해주세요.<br>감사합니다.
+              <div style="margin-top: 10px">${redirectURL}</div>
               </div>
             </div>
           </td>
