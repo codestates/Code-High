55032708031de0d52500c36b7bb185aecff075be
soyou <loverduck97@naver.com>
2021-10-07 16:58:38 +0900

    [Server] fix: token regenerate

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,8 +1,31 @@
 import { NextFunction, Request, Response } from 'express';
+import { regenerateToken } from '../controllers/auth';
+import { userInfo } from '../controllers/user';
+import { User } from '../entity/User';
+import { checkToRegenerate } from '../utils/jwt';
 
-export const errorHandler = (err, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = async (err, req: Request, res: Response, next: NextFunction) => {
   if (err === 'InvalidToken') {
-    return res.status(401).send('invalid Token');
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      return res.status(401).send('invalid Token');
+    }
+
+    const verified = await checkToRegenerate(refreshToken);
+    if (!verified) {
+      return res.status(401).send('invalid refresh token');
+    }
+
+    await User.update(verified.id, { refreshToken: verified.refreshToken });
+
+    res.clearCookie('refreshToken');
+    res.cookie('refreshToken', verified.refreshToken, {
+      maxAge: 1000 * 60 * 60 * 24 * 14, // 14d
+      httpOnly: true,
+      secure: true,
+    },)
+    
+    return res.status(401).send({ accessToken: verified.accessToken });
   }
   if (err === 'NotFoundTokenUser') {
     return res.status(401).send('cannot find Token user');
