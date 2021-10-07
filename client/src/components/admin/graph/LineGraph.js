d4d38836dad660460bb1df1a4ce3d9b655207f0e
soowan-jang <jangswn2@gmail.com>
2021-10-07 13:32:01 +0900

    [Client] #221 fix:mypage css

--- client/src/components/admin/graph/LineGraph.js
+++ client/src/components/admin/graph/LineGraph.js
@@ -35,7 +35,7 @@ function LineGraph() {
   }
 
   const options = {
-    responsive: false,
+    // responsive: false,
     maintainAspectRatio: false,
     scales: {
       y: {
@@ -49,7 +49,7 @@ function LineGraph() {
 
   return (
     <div className='line-graph-container'>
-      <Line className='line-graph' width='920px' height='325px' style={{padding:10, marginTop:10}}  data={chartData} options={options} />
+      <Line className='line-graph' width='910px' height='310px' data={chartData} options={options} />
     </div>
   )
 }
